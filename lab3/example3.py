f10ecddb8c744b4d919d34eba7e0260c6d58a10d
r87007541 <fatihguzel220122@gmail.com>
2020-10-26 09:10:23 +0000

    ticket discount

---
+++ lab3/example3.py
@@ -0,0 +1,5 @@
+gpa = float(input("gpa :"))
+if gpa < 2.0 :
+   print("Not enough GPA!")
+else :
+  print("Graduated")
---
+++ lab3/example4.py
@@ -0,0 +1,10 @@
+age = int(input("Your Age :"))
+normal_bus_ticket = 3
+if age > 60 :
+  print((normal_bus_ticket)*0)
+elif age < 6 :
+  print((normal_bus_ticket)*0)
+elif 6 <= age <= 18 :
+  print((normal_bus_ticket)*0.5)
+else :
+  print(normal_bus_ticket)

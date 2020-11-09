1a7161fe9bfd8068bf90662b8eb5c5313e9c36b7
r87007541 <fatihguzel220122@gmail.com>
2020-11-09 09:32:44 +0000

    factorial

---
+++ lab4/example4.py
@@ -0,0 +1,3 @@
+a = int(input("Enter a Number :"))
+b = int(input("Enter a Number :"))
+for x in range(b)
---
+++ lab4/example5.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a Number :"))
+factorial = 1
+for x in range(1,n+1) :
+  factorial *= x
+print(factorial)

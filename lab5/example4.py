72f5125e5eb186e91c61f2c791b1f2f75e08451e
r87007541 <fatihguzel220122@gmail.com>
2020-11-16 09:46:11 +0000

    Password

--- lab5/example4.py
+++ lab5/example4.py
@@ -1,8 +1,10 @@
 password = "fth123"
 while True :
   user_pass = input("Enter the Password : ")
-  if password != user_pass :  
+  if user_pass == "help" :
+    print(password[0])
+  elif password != user_pass :  
     print("Wrong")
   else :
     print("Welcome")
-  break
+    break

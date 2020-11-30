e5fdd4a1fa8fd8da60081f7badc070db573eded9
r87007541 <fatihguzel220122@gmail.com>
2020-11-30 10:31:40 +0000

    employees

--- lab7/example3.py
+++ lab7/example3.py
@@ -1,13 +1,17 @@
 names = []
 salaries = []
+employees = {}
 for i in range(5) :
   name = input("Enter the employee's name :\n")
-  salary = input("Enter the employee's salary :\n")
+  salary = int(input("Enter the employee's salary :\n"))
   names.append(name)
   salaries.append(salary)
 for x,y in zip(names , salaries) :
   print(x)
   print(y)
-  employees = {}
   employees[x] = y
-print(employees)
+sorted_salaries = sorted(employees.values())
+for x in employees.keys() :
+  if employees[x] in sorted_salaries[2:] :
+    print(x)
+    print(employees[x])
